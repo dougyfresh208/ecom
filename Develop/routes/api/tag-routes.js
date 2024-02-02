de27f18603110f265e43f791c1a20c6636ace333
SteveR <steve17rouse@gmail.com>
2024-02-02 14:59:25 -0700

    updated file -- added data fields

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,28 +1,84 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
-
-// The `/api/tags` endpoint
-
+// GET all tags with their associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product],
+  })
+    .then((tags) => res.json(tags))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// GET a single tag by its ID with its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [Product],
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// POST a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((tag) => res.status(201).json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
+// PUT to update a tag's name by its ID
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: 'Tag was updated successfully.' });
+      } else {
+        res.send({ message: `Cannot update Tag with id=${req.params.id}. Maybe Tag was not found or req.body is empty!` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error updating Tag with id=' + req.params.id,
+      });
+    });
 });
 
+// DELETE a tag by its ID
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: 'Tag was deleted successfully!' });
+      } else {
+        res.send({ message: `Cannot delete Tag with id=${req.params.id}. Maybe Tag was not found!` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Could not delete Tag with id=' + req.params.id,
+      });
+    });
 });
 
 module.exports = router;
+
