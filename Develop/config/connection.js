cfb2109d4db16c98c7bc8c4fbfc3ea754242edc7
SteveR <steve17rouse@gmail.com>
2024-02-02 14:34:11 -0700

    updated files

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.ecom, process.env.root, process.env.boise001, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
