14a902eeee82d146e9c1e6e9a6a282e52e55ab79
SteveR <steve17rouse@gmail.com>
2024-02-02 14:58:25 -0700

    updated file -- added data fields

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,87 +1,19 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
-
-// GET all categories with their associated Products
-router.get('/', (req, res) => {
-  Category.findAll({
-    include: [Product],
-  })
-    .then((categories) => res.json(categories))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// GET a single category by its ID with its associated Products
-router.get('/:id', (req, res) => {
-  Category.findByPk(req.params.id, {
-    include: [Product],
-  })
-    .then((category) => {
-      if (!category) {
-        res.status(404).json({ message: 'No category found with this id' });
-        return;
-      }
-      res.json(category);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// POST a new category
-router.post('/', (req, res) => {
-  Category.create({
-    category_name: req.body.category_name,
-  })
-    .then((category) => res.status(201).json(category))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
-
-// PUT to update a category by its ID
-router.put('/:id', (req, res) => {
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({ message: 'Category was updated successfully.' });
-      } else {
-        res.send({ message: `Cannot update Category with id=${req.params.id}. Maybe Category was not found or req.body is empty!` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: 'Error updating Category with id=' + req.params.id,
-      });
-    });
-});
-
-// DELETE a category by its ID
+// DELETE a product by its ID
 router.delete('/:id', (req, res) => {
-  Category.destroy({
+  Product.destroy({
     where: { id: req.params.id },
   })
     .then((num) => {
       if (num == 1) {
-        res.send({ message: 'Category was deleted successfully!' });
+        res.send({ message: 'Product was deleted successfully!' });
       } else {
-        res.send({ message: `Cannot delete Category with id=${req.params.id}. Maybe Category was not found!` });
+        res.send({ message: `Cannot delete Product with id=${req.params.id}. Maybe Product was not found!` });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: 'Could not delete Category with id=' + req.params.id,
+        message: 'Could not delete Product with id=' + req.params.id,
       });
     });
 });
-
-module.exports = router;
 
