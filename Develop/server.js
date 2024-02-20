cdacfc75a0a83724fe5cf9dd9f6886a372c0d626
SteveR <steve17rouse@gmail.com>
2024-02-20 14:28:29 -0700

    removed comments

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const routes = require('./routes');
-// const sequelize = require('./config/connection'); // Ensure this path is correct
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(routes);
-
-// // sync sequelize models to the database, then turn on the server
-// sequelize.sync({ force: true }).then(() => {
-//   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-// });
-
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection'); // Adjust the path if necessary
